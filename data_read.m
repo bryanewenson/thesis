43bd570b1b422ef925fa424588f3612e51c05379
bryanewenson <bryanewenson@hotmail.com>
2019-04-02 21:54:50 -0300

    Added subsampling functionality

--- data_read.m
+++ data_read.m
@@ -1,27 +1,34 @@
-function [D, F, L, num_useful_features] = data_read(dataset, sort_order)
+function [D, F, L, num_useful_features] = data_read(dataset, sort_order, ss_portion)
 %UNTITLED4 Summary of this function goes here
 %   Detailed explanation goes here
     try
         load(dataset);
 
-        %Find the indices of all useless features for removal
-        U = arrayfun(@(col) find_useless(measurements(:,col), samples * 0.50), (1:num_features))';
+        ss_pos_samples = round(positive_samples * ss_portion); 
+        ss_pos_indices = positive_indices(randsample(positive_samples, ss_pos_samples));
+        ss_neg_samples = round(negative_samples * ss_portion);
+        ss_neg_indices = negative_indices(randsample(negative_samples, ss_neg_samples));
 
-        measurements(:,U) = [];
-        num_features = size(measurements, 2);
-        L = labels;
+        F = [measurements(ss_pos_indices, :);measurements(ss_neg_indices, :)];
+        L = [labels(ss_pos_indices);labels(ss_neg_indices)];
+
+        positive_indices = 1:ss_pos_samples;
+        negative_indices = (ss_pos_samples + 1):ss_pos_samples + ss_neg_samples;
+        
+        %Find the indices of all useless features for removal
+        U = arrayfun(@(col) find_useless(F(:,col), samples * 0.50), (1:num_features))';
+
+        F(:,U) = [];
+        num_features = size(F, 2);
 
         %Calculate Cohens D for the remaining features
-        D = arrayfun(@(col) get_effect_size(measurements(:,col), positive_indices, negative_indices), (1:num_features))';
+        D = arrayfun(@(col) get_effect_size(F(:,col), positive_indices, negative_indices), (1:num_features))';
         D(isnan(D) | D == 0) = [];
 
         try
             [D, sorted_indices] = sort(D, sort_order);
-            min_idx = 21;
-            max_idx = 40;
-            num_useful_features = size(sorted_indices,1);
-            feature_range = min_idx:max_idx;
-            F = measurements(:,sorted_indices);
+            num_useful_features = size(D,1);
+            F = F(:,sorted_indices);
         catch
            disp('Invalid sort order');
 
