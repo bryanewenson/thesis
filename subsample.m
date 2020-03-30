0023a3b52537ea68e11d78b9832f6cfaeac66649
bryanewenson <bryanewenson@hotmail.com>
2020-03-30 08:41:11 -0400

    Fixed bug affecting stratification of samples

--- subsample.m
+++ subsample.m
@@ -3,17 +3,17 @@ function S_idx = subsample(L, ssp)
     neg_idx = find(L == 0);
     neg_samples = size(neg_idx,1);
     neg_ssp_samples = round(neg_samples * ssp);
-    neg_idx = randsample(neg_samples, neg_ssp_samples);
+    neg_idx = randsample(neg_idx, neg_ssp_samples);
     
     pos_idx = find(L == 1);
     pos_samples = size(pos_idx,1);
     pos_ssp_samples = round(pos_samples * ssp);
-    pos_idx = randsample(pos_samples, pos_ssp_samples);
+    pos_idx = randsample(pos_idx, pos_ssp_samples);
 
     S_idx = [neg_idx;pos_idx];
     
     %Randomize the order of the indices
     S_idx = S_idx(randperm(neg_ssp_samples + pos_ssp_samples));
     
-end
+S_idxend
 
